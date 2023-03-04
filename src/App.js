839280c0b83cb8ff9a78def0002272a8141225de
Olar-Alex-Student <117835182+Olar-Alex-Student@users.noreply.github.com>
2023-03-04 12:30:37 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> merge edit
+          Edit <code>src/App.js</code> Dorin e boss
         </p>
         <a
           className="App-link"
